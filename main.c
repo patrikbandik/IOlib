b12bcfaa97b7ef3ed01cfa3ddfa3bdb65da9efb9
patrikbandik <bandik.patrik@gmail.com>
2020-10-12 10:47:34 +0000

    working pinMode func

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 
-//void pinMode(unsigned char portname, int portnum, int flag);
-
 unsigned char DDRB = 0b00000000;
 void pinMode(char portname,int portnum, int flag){
   switch(portname){
     case 'B':
-      DDRB ^= (flag ^ DDRB) & (1UL << portnum);
+      DDRB ^= (-flag ^ DDRB) & (1UL << portnum);
   }
 
 }
@@ -18,8 +16,7 @@ int main(){
     for(int i = 0; i <= 2; i++){
         printf("%d\t", arr[i]);
     }
-    return 0; */
-
+    return 0; 
     unsigned char number = 0b00000000;
     unsigned long newbit = !!1;    // Also booleanize to force 0 or 1
     number ^= (-newbit ^ number) & (1 << 3);
@@ -30,9 +27,28 @@ int main(){
     number ^= (-newbit ^ number) & (1UL << 1);
     printf("%d\n",number);
     number = (number & ~(1UL << 3)) | (newbit << 3);
-    printf("%d\n",number);
+    printf("%d\n",number); */
+
 
     pinMode('B',0, 1);
-    printf("%d",DDRB);
+    printf("%d\n",DDRB);
+    pinMode('B',0, 0);
+    printf("%d\n",DDRB);
+    pinMode('B',0, 1);
+    printf("%d\n",DDRB);
+    pinMode('B',1, 1);
+    printf("%d\n",DDRB);
+    pinMode('B',2, 1);
+    printf("%d\n",DDRB);
+    pinMode('B',3, 1);
+    printf("%d\n",DDRB);
+    pinMode('B',0, 0);
+    printf("%d\n",DDRB);
+    pinMode('B',1, 0);
+    printf("%d\n",DDRB);
+    pinMode('B',2, 0);
+    printf("%d\n",DDRB);
+    pinMode('B',3, 0);
+    printf("%d\n",DDRB);
     }
 
